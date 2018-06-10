a5baabc90c0de63d5bab34b8c1a5ab56439cc340
sschmidt23 <sschmidt@physics.ucdavis.edu>
2018-06-10 15:53:05 -0700

    correct tests

--- tests.py
+++ tests.py
@@ -1,13 +1,12 @@
 import unittest
-
 import code
 
 class TestMethods(unittest.TestCase):
     def test_mag(self):
-        self.assertEqual(code.magnitude(100.,0.),5.)
+        self.assertAlmostEqual(code.magnitude(100.,0.),-5.)
 
     def test_flux(self):
-        self.assertEqual(code.flux(5.),100.)
+        self.assertAlmostEqual(code.flux(-5.),100.)
 
 if __name__=="__main__":
     unittest.main()
