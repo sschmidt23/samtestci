deca19b0426d636c1a41ed4b2f85af8e0ad172a9
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-07-31 14:17:57 -0700

    use pytest as library

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,12 +1,12 @@
-from samtestci import flux, magnitude, fibonacci
+from code import flux, magnitude, fibonacci
 import numpy as np
 
 
 def fibo_test():
-    assert fibonacci(2) == 2
+    assert fibonacci(2) == 1
 
 def fluxtest():
-    assert np.isclose(flux(5.),100.)
+    assert np.isclose(flux(-5.),100.)
 
 def magtest():
-    assert np.isclose(magnitude(100.), 5.)
+    assert np.isclose(magnitude(100.), -5.)
